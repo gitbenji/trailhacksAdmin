d6a2d360aab7e816458cc6a333a317fe79e237d8
shvagahb <biancaaidass@gmail.com>
2016-04-24 10:41:52 -0400

    live map on login & create

--- create.js
+++ create.js
@@ -1,3 +1,6 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiYnNoYW5rd2l0eiIsImEiOiJjaW14cHA0YTcwM2x2dXdtNGN1dDc5OXN0In0.URoO3IYb5g29cxUykBZ9Aw';
+var map = L.mapbox.map('map', 'mapbox.streets')
+.setView([30.433832, -84.290650], 12);
 
 
 $('#submit').on('click',function(){
