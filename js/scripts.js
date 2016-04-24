c44ee229eef9abc99dadce96d548ad624ea206cc
shvagahb <biancaaidass@gmail.com>
2016-04-24 03:15:03 -0400

    marker name sticks to each marker

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,39 @@
 
 // to be filled with GET request
 var markerArr = [];
+var activeMarker;
 var i = markerArr.length;
 
 L.mapbox.accessToken = 'pk.eyJ1IjoiYnNoYW5rd2l0eiIsImEiOiJjaW14cHA0YTcwM2x2dXdtNGN1dDc5OXN0In0.URoO3IYb5g29cxUykBZ9Aw';
 var map = L.mapbox.map('map', 'mapbox.streets')
 .setView([30.433832, -84.290650],12);
 
-// fill in the html for latitude and longitude of the sidebar
+// fill in the input values of the sidebar
+function fillSidebar(markerObject) {
+  $('#markerName').val(markerObject.markerName);
+  $('#trailName').val(markerObject.trailName);
+  $('#latitude').val(markerObject.lat);
+  $('#longitude').val(markerObject.lng);
+  $('#summary').val(markerObject.summaryVal);
+}
+
 function fillSidebarLatLng(lat, lng) {
   $('#latitude').val(lat);
   $('#longitude').val(lng);
 }
 
-
-// function fillSidebarNames()
-// document.getElementsByClassName('markerName')[0].innerHTML =
-
-
-function createMarker (lat,lng){
+function createMarker(lat,lng) {
   var markerVar = L.marker(new L.LatLng(lat, lng), {
       icon: L.mapbox.marker.icon({
           'marker-color': 'ff8888'
       }),
       draggable: true
   });
-  markerVar.bindPopup('This marker is draggable! Move it around.');
+  // markerVar.bindPopup('poop');
   markerVar.addTo(map);
-
 
   markerVar.id = i;
   i++;
-
 
   return markerVar;
 }
@@ -41,31 +43,25 @@ map.on('click', function(e) {
     var lat = e.latlng.lat;
     var lng = e.latlng.lng;
 
-
     fillSidebarLatLng(lat, lng);
 
     var markerVar = createMarker(lat, lng);
-    console.log(markerVar);
+    activeMarker = markerVar.id;
     var markerObject = new Object();
-    markerObject.markerName = $("#markerName").val();
-    markerObject.trailName = $("#trailName").val();
-    markerObject.loopName = $("#loopName").val();
-    markerObject.lat = e.latlng.lat;
-    markerObject.lng = e.latlng.lng;
-    markerObject.summaryVal = $("#summary").val();
-    console.log(markerObject);
-
+    markerObject.lat = lat;
+    markerObject.lng = lng;
     markerArr.push(markerObject);
+
+    fillSidebar(markerObject);
+
     markerVar.on('click', function(e) {
-      var lat = e.latlng.lat;
-      var lng = e.latlng.lng;
-      var index = e.target.id;
-      var markerObject = markerArr[index];
-      console.log(markerObject);
+      //active marker updates to id of click marker
+      activeMarker = e.target.id;
 
-      console.log(e.target.id);
+      var markerObject = markerArr[activeMarker];
+      console.log(activeMarker, markerObject);
 
-      fillSidebarLatLng(lat, lng);
+      fillSidebar(markerObject);
     });
 
     $('#sidebar').removeClass('side-hidden');
@@ -74,25 +70,39 @@ map.on('click', function(e) {
 
 
 $('#button').on('click', function() {
-  var summaryVal = $('#summary').val();
 
-  $.ajax('http://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    data: {
-      title: 'foo',
-      body: summaryVal,
-      userId: 1
-    }
-  }).then(function(data) {
-    console.log(data);
-  });
+  var markerObject = markerArr[activeMarker];
+  console.log(activeMarker, markerObject);
+
+  markerObject.markerName = $("#markerName").val();
+  markerObject.trailName = $("#trailName").val();
+  markerObject.loopName = $("#loopName").val();
+  markerObject.lat = $("#latitude").val();
+  markerObject.lng = $("#longitude").val();
+  markerObject.summaryVal = $("#summary").val();
+
+  markerArr[activeMarker] =  markerObject;
+  console.log(markerObject);
+  console.log(markerArr);
+
+
+  // $.ajax('http://jsonplaceholder.typicode.com/posts', {
+  //   method: 'POST',
+  //   data: {
+  //     title: 'foo',
+  //     body: summaryVal,
+  //     userId: 1
+  //   }
+  // }).then(function(data) {
+  //   console.log(data);
+  // });
 
 });
 
-$(document).ready(function() {
-  // Sidebar stuff
-  $('#test-click').on('click', function() {
-    $('#sidebar').toggleClass('side-hidden');
-  });
-
-});
+// $(document).ready(function() {
+//   // Sidebar stuff
+//   $('#test-click').on('click', function() {
+//     $('#sidebar').toggleClass('side-hidden');
+//   });
+//
+// });
