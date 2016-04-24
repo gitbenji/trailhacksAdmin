21ddccf1c025e0c9ec617e190741b89cc7c4ae45
shvagahb <biancaaidass@gmail.com>
2016-04-23 22:51:32 -0400

    map/marker scope

--- js/scripts.js
+++ js/scripts.js
@@ -3,32 +3,24 @@ L.mapbox.accessToken = 'pk.eyJ1IjoiYnNoYW5rd2l0eiIsImEiOiJjaW14cHA0YTcwM2x2dXdtN
 var map = L.mapbox.map('map', 'mapbox.streets')
 .setView([30.433832, -84.290650],12);
 
-var marker = L.marker(new L.LatLng(30.445895, -84.218511), {
-    icon: L.mapbox.marker.icon({
-        'marker-color': 'ff8888'
-    }),
-    draggable: true
-});
-marker.bindPopup('This marker is draggable! Move it around.');
-marker.addTo(map);
-
-
-marker.on('click',function(e){
-  console.log(e.latlng);
-  var lat = e.latlng.lat;
-  var lng = e.latlng.lng;
-
-  fillSidebarLatLng(lat, lng);
-
-});
-
 // fill in the html for latitude and longitude of the sidebar
 function fillSidebarLatLng(lat, lng) {
   document.getElementsByClassName('latitude')[0].innerHTML = "Latitude: " + lat;
   document.getElementsByClassName('longitude')[0].innerHTML = "Longitude: " + lng;
 }
 
+function createMarker (lat,lng){
+  var markerVar = L.marker(new L.LatLng(lat, lng), {
+      icon: L.mapbox.marker.icon({
+          'marker-color': 'ff8888'
+      }),
+      draggable: true
+  });
+  markerVar.bindPopup('This marker is draggable! Move it around.');
+  markerVar.addTo(map);
 
+  return markerVar;
+}
 
 // grab lat & lon on click
 map.on('click', function(e) {
@@ -37,14 +29,15 @@ map.on('click', function(e) {
 
     fillSidebarLatLng(lat, lng);
 
-    var marker = L.marker(new L.LatLng(lat, lng), {
-        icon: L.mapbox.marker.icon({
-            'marker-color': 'ff8888'
-        }),
-        draggable: true
+    var markerVar = createMarker(lat, lng);
+
+    markerVar.on('click',function(e) {
+      console.log(e.latlng);
+      var lat = e.latlng.lat;
+      var lng = e.latlng.lng;
+
+      fillSidebarLatLng(lat, lng);
     });
-    marker.bindPopup('This marker is draggable! Move it around.');
-    marker.addTo(map);
 
     $('#sidebar').removeClass('side-hidden');
 });
