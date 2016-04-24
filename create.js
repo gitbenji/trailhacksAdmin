62c3cf5418bd609a7fd84d3e3e688b3faeb80903
Benji Shankwitz <benji@Benjis-MacBook-Pro.local>
2016-04-24 08:16:25 -0400

    sidebar

--- create.js
+++ create.js
@@ -4,6 +4,7 @@ $('#submit').on('click',function(){
   var loginObject = new Object();
   loginObject.email = $('#email').val();
   loginObject.password = $('#password').val();
+  console.log('click');
   $.ajax ({
       url: "https://trailhacks-api.herokuapp.com/users",
       crossDomain: true,
