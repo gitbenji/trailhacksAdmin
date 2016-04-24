04a25223ffd4c177f40f20eaed700658574a7ff2
shvagahb <biancaaidass@gmail.com>
2016-04-24 06:01:11 -0400

    login

--- login.js
+++ login.js
@@ -1,13 +1,14 @@
-var loginObject = new Object();
-loginObject.email = $('#email').val();
-loginObject.password = $('#password').val();
+
 
 $('#submit').on('click',function(){
+  var loginObject = new Object();
+  loginObject.email = $('#email').val();
+  loginObject.password = $('#password').val();
 $.ajax ({
     url: "http://www.trailhacks-api.herokuapp.com/users/authenticate",
     type: "POST",
     data: JSON.stringify(loginObject),
-    contentType: "application/json",
+    dataType: "jsonp",
     success: function(result) {
       console.log('result');
       // window.location.href = "index.html";
